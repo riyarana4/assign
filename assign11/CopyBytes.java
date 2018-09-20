3b171ca769a892c144a9ab142263553958d273c3
riyarana4 <42269184+riyarana4@users.noreply.github.com>
2018-09-20 20:33:17 +0530

    Assignment11

---
+++ assign11/CopyBytes.java
@@ -0,0 +1,34 @@
+import java.io.*;
+class CopyBytes{
+public static void main(String[] args) throws IOException
+{
+FileInputStream in=null;
+FileOutputStream out=null;
+try
+{
+in=new FileInputStream("abc.txt");
+out=new FileOutputStream("hello.txt");
+int c;
+while((c=in.read())!=-1)
+{
+out.write(c);
+}
+}
+finally{
+if(in!=null)
+{
+in.close();
+}
+if(out!=null)
+{
+out.close();
+}
+}
+in=new FileInputStream("hello.txt");
+int c;
+while((c=in.read())!=-1)
+{
+System.out.print((char)c);
+}
+}
+}
